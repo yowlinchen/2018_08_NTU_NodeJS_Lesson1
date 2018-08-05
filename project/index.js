fb3b7e5e49d965011aedc54721fbe790551dd88c
yowlinchen <terry.y.chen@intel.com>
2018-08-05 18:12:28 +0800

    first backend website that front end can connect

--- project/index.js
+++ project/index.js
@@ -1,3 +1,12 @@
 // can't use HTML DOM, for example alert()
-let x = 3;
-console.log(x);
+// let x = 3;
+// console.log(x);
+
+// load express package 
+let express = require("express");
+// create applicatoin object
+let app = express();
+// start the server at http://localhost:3000/
+app.listen(3000, function () {
+    console.log("Server Started");
+})
